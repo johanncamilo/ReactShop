0b96ae0d1e6c7749fbf9feab110ac8ee6931dae4
Johann Camilo Belosareos Colmenares <milodevcool@gmail.com>
2023-12-15 18:22:03 -0500

    Declarative & Conditional Rendering

--- src/Components/Navbar/index.jsx
+++ src/Components/Navbar/index.jsx
@@ -3,62 +3,54 @@ import { NavLink } from 'react-router-dom'
 export const Navbar = () => {
   const activeStyle = 'underline underline-offset-4'
 
+  const leftnav = [
+    { to: '/', text: 'Shope', className: 'font-semibold text-lg' },
+    { to: '/', text: 'All', className: '' },
+    { to: '/clothes', text: 'Clothes', className: '' },
+    { to: '/electronics', text: 'Electronics', className: '' },
+    { to: '/furniture', text: 'Furniture', className: '' },
+    { to: '/toys', text: 'Toys', className: '' },
+    { to: '/others', text: 'Others', className: '' },
+  ]
+
+  const rightnav = [
+    { to: undefined, text: 'milodevcool@gmail.com', className: 'text-black/60' },
+    { to: '/my-orders', text: 'my-orders', className: '' },
+    { to: '/my-account', text: 'my-account', className: '' },
+    { to: '/sign-in', text: 'sign-in', className: '' },
+    { to: undefined, text: '🛒 0', className: '' },
+  ]
+
+  const NavLinkClass = (isActive) => (isActive ? activeStyle : undefined)
+
   return (
     <nav className='flex justify-between items-center fixed z-10 w-full py-5 px-8 text-sm font-light'>
       <ul className='flex items-center gap-3'>
-        <li className='font-semibold text-lg'>
-          <NavLink to='/'>Shope</NavLink>
-        </li>
-        <li>
-          <NavLink to='/' className={({ isActive }) => (isActive ? activeStyle : undefined)}>
-            All
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/clothes' className={({ isActive }) => (isActive ? activeStyle : undefined)}>
-            Clothes
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/electronics' className={({ isActive }) => (isActive ? activeStyle : undefined)}>
-            Electronics
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/furniture' className={({ isActive }) => (isActive ? activeStyle : undefined)}>
-            Furniture
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/toys' className={({ isActive }) => (isActive ? activeStyle : undefined)}>
-            Toys
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/others' className={({ isActive }) => (isActive ? activeStyle : undefined)}>
-            Others
-          </NavLink>
-        </li>
+        {leftnav.map(({ to, text, className }) => {
+          return (
+            <li key={text} className={className}>
+              <NavLink to={to} className={text === 'Shope' ? '' : ({ isActive }) => NavLinkClass(isActive)}>
+                {text}
+              </NavLink>
+            </li>
+          )
+        })}
       </ul>
 
       <ul className='flex items-center gap-3'>
-        <li className='text-black/60'>milodevcool@gmail.com</li>
-        <li>
-          <NavLink to='/my-orders' className={({ isActive }) => (isActive ? activeStyle : undefined)}>
-            My Orders
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/my-account' className={({ isActive }) => (isActive ? activeStyle : undefined)}>
-            My Account
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-in' className={({ isActive }) => (isActive ? activeStyle : undefined)}>
-            Sign in
-          </NavLink>
-        </li>
-        <li>🛒 0</li>
+        {rightnav.map(({ to, text, className }) => {
+          return (
+            <li key={text} className={className}>
+              {to ? (
+                <NavLink to={to} className={({ isActive }) => NavLinkClass(isActive)}>
+                  {text}
+                </NavLink>
+              ) : (
+                <>{text}</>
+              )}
+            </li>
+          )
+        })}
       </ul>
     </nav>
   )
