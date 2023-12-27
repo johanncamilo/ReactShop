810be5a099f43e58f2fe8dbdb097e36bd2237000
Johann Camilo Belosareos Colmenares <milodevcool@gmail.com>
2023-12-27 13:08:40 -0500

    Styles in OrdersCard + ChevronRightIcon

--- src/Components/OrdersCard/index.jsx
+++ src/Components/OrdersCard/index.jsx
@@ -1,14 +1,21 @@
-// import { XCircleIcon } from '@heroicons/react/24/solid'
+import { ChevronRightIcon } from '@heroicons/react/24/solid'
 
 const OrdersCard = ({ totalPrice, totalProducts }) => {
   const date = new Date()
   return (
-    <div className='flex justify-between items-center mb-3 border border-black'>
-      <p>
-        <span>{date.toLocaleDateString()}</span>&nbsp;
-        <span>{totalProducts}</span>&nbsp;
-        <span>{totalPrice}</span>&nbsp;
-      </p>
+    <div className='flex justify-center items-center mb-3 border border-black p-4 w-80 rounded-lg'>
+      <div className='flex justify-between w-full'>
+        <p className='flex flex-col font-light'>
+          <span>{date.toLocaleDateString()}</span>
+          <span>
+            {totalProducts} {totalProducts === 1 ? 'article' : 'articles'}
+          </span>
+        </p>
+        <p className='flex items-center gap-2'>
+          <span className='font-medium text-2xl'>${totalPrice}</span>
+          <ChevronRightIcon className='h-6 w-6 text-black-500 cursor-pointer' />
+        </p>
+      </div>
     </div>
   )
 }
